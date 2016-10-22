188740715a699c25651a74f3ce090aa130127680
Josh Kramer <jkjustjoshing@gmail.com>
2016-10-21 22:03:46 -0400

    Delete any existing selected text when inserting text

--- src/commands.ts
+++ src/commands.ts
@@ -13,9 +13,10 @@ export function runCommand (command: string) {
     editor.edit(editBuilder => {
         if (command === '1toX') {
             let orderedSelections = lodashSortBy(selections, (selection: vscode.Selection) => selection.start.line);
-            orderedSelections.forEach((location, index) => {
-                let range = new vscode.Position(location.start.line, location.start.character);
+            orderedSelections.forEach((selection, index) => {
+                let range = new vscode.Position(selection.start.line, selection.start.character);
                 editBuilder.insert(range, String(index + 1));
+                editBuilder.delete(selection);
             });
         }
     });
