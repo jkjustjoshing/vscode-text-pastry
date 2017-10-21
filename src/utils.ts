620fa3b448122e2aa25566b5079373364d8b6883
Josh Kramer <jkjustjoshing@gmail.com>
2017-10-20 20:52:02 -0400

    Add copy-paste node module and use to get lines of text from clipboard.

--- src/utils.ts
+++ src/utils.ts
@@ -2,10 +2,24 @@
 
 import * as vscode from 'vscode';
 const lodashSortBy = require('lodash.sortby');
+const copyPaste = require('copy-paste');
 
 export function getCursors (editBuilder): vscode.Selection[] {
     const editor = vscode.window.activeTextEditor;
     const { document, selections } = editor;
 
     return lodashSortBy(selections, [ 'start.line', 'start.character' ]);
+}
+
+export function getClipboardLines (): Promise<string[]> {
+    return new Promise(function (resolve) {
+        copyPaste.paste(function (err, val: string) {
+            if (typeof val !== 'string') {
+                resolve([]);
+            }
+            else {
+                resolve(val.replace('\r\n', '\n').split('\n'));
+            }
+        });
+    });
 }
