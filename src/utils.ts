07b56b575bd26e0a776259c263215d22dd3ee189
Josh Kramer <josh@kramer.run>
2022-10-18 20:52:41 -0400

    Use vscode api method to get clipboard. Remove "copy-paste" library dependency

--- src/utils.ts
+++ src/utils.ts
@@ -1,24 +1,20 @@
 "use strict";
 
-import * as vscode from "vscode";
+import { env, Selection, window } from "vscode";
 const lodashSortBy = require("lodash.sortby");
-const copyPaste = require("copy-paste");
 
-export function getCursors(editBuilder): vscode.Selection[] {
-	const editor = vscode.window.activeTextEditor;
+export function getCursors(editBuilder): Selection[] {
+	const editor = window.activeTextEditor;
 	const { document, selections } = editor;
 
 	return lodashSortBy(selections, ["start.line", "start.character"]);
 }
 
-export function getClipboardLines(): Promise<string[]> {
-	return new Promise(function (resolve) {
-		copyPaste.paste(function (err, val: string) {
-			if (typeof val !== "string") {
-				resolve([]);
-			} else {
-				resolve(val.replace("\r\n", "\n").split("\n"));
-			}
-		});
-	});
+export async function getClipboardLines(): Promise<string[]> {
+	const val = await env.clipboard.readText();
+	if (typeof val !== "string") {
+		return [];
+	} else {
+		return val.replace("\r\n", "\n").split("\n");
+	}
 }
